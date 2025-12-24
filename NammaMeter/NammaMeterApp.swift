a6c296b9da195c58b503bf22908c1a18523adb2a
sridhar <sridharsm@live.com>
2025-12-24 18:27:38 +0530

    Initial Commit

---
+++ NammaMeter/NammaMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NammaMeterApp.swift
+//  NammaMeter
+//
+//  Created by sridhar on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NammaMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
