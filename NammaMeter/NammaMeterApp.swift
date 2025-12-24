5efeca1ba10827fb3418afecfcece7970c467fc5
sridhar <sridharsm@live.com>
2025-12-24 18:29:50 +0530

    [Sources] Adjust tab width

--- NammaMeter/NammaMeterApp.swift
+++ NammaMeter/NammaMeterApp.swift
@@ -1,17 +1,10 @@
-//
-//  NammaMeterApp.swift
-//  NammaMeter
-//
-//  Created by sridhar on 12/24/25.
-//
-
 import SwiftUI
 
 @main
 struct NammaMeterApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
